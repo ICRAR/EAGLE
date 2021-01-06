39984f8149eb2ee6f08036420c3f423d18388cd6
james-strauss-uwa <james.strauss@uwa.edu.au>
2021-01-06 17:36:54 +0800

    Renamed collapseNodes to collapseGroups. Added a new button for collapseNodes.

--- src/Eagle.ts
+++ src/Eagle.ts
@@ -1535,8 +1535,8 @@ export class Eagle {
         }
     };
 
-    toggleCollapseAllNodes = () : void => {
-        console.log("toggleCollapseAllNodes");
+    toggleCollapseAllGroups = () : void => {
+        console.log("toggleCollapseAllGroups");
 
         // first work out whether we should be collapsing or expanding
         let numCollapsed: number = 0;
@@ -1559,6 +1559,37 @@ export class Eagle {
             let node: Node = this.logicalGraph().getNodes()[i];
 
             if (node.isGroup()){
+                node.setCollapsed(collapse);
+            }
+        }
+
+        this.flagActiveDiagramHasMutated();
+    }
+
+    toggleCollapseAllNodes = () : void => {
+        console.log("toggleCollapseAllNodes");
+
+        // first work out whether we should be collapsing or expanding
+        let numCollapsed: number = 0;
+        let numExpanded: number = 0;
+        for (let i = 0 ; i < this.logicalGraph().getNodes().length ; i++){
+            let node: Node = this.logicalGraph().getNodes()[i];
+
+            if (!node.isGroup()){
+                if (node.isCollapsed()){
+                    numCollapsed += 1;
+                } else {
+                    numExpanded += 1;
+                }
+            }
+        }
+        let collapse: boolean = numExpanded > numCollapsed;
+
+        // now loop through and collapse or expand all group nodes
+        for (let i = 0 ; i < this.logicalGraph().getNodes().length ; i++){
+            let node: Node = this.logicalGraph().getNodes()[i];
+
+            if (!node.isGroup()){
                 node.setCollapsed(collapse);
             }
         }
