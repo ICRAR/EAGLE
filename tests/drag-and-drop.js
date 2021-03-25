ba4356965500e35375efdd0f11b6166b4168ca3d
james-strauss-uwa <james.strauss@uwa.edu.au>
2021-03-25 14:07:55 +0800

    Added simple test of node drag-and-drop functionality

---
+++ tests/drag-and-drop.js
@@ -0,0 +1,30 @@
+import { Selector } from 'testcafe';
+
+/*
+    run with:
+
+    export EAGLE_GITHUB_ACCESS_TOKEN="<token>";testcafe chrome tests/drag-and-drop.js
+*/
+
+var EAGLE_GITHUB_ACCESS_TOKEN = process.env.EAGLE_GITHUB_ACCESS_TOKEN;
+
+fixture `EAGLE Drag and Drop Node`
+    .page `http://localhost:8888/`
+
+test('Drag and drop node', async t =>{
+    await t
+        // wait for the page to settle down
+        .wait(3000)
+
+        .drag(Selector('#addPaletteNode8'), 250, 0, {
+            offsetX: 40,
+            offsetY: 40
+        })
+        .wait(250)
+
+        // check that a file node has been created
+        .expect(Selector("#nodeNameValue").value).eql("File")
+
+        // end
+        .wait(3000);
+});
