c89bffc8844b4915d81641d509f058239b00d0ab
james-strauss-uwa <james.strauss@uwa.edu.au>
2021-03-31 13:41:21 +0800

    Added new test that attempts to edit value of node parameter in the node inspector UI.

---
+++ tests/edit-node-parameter.js
@@ -0,0 +1,34 @@
+import { Selector } from 'testcafe';
+
+/*
+    run with:
+
+    export EAGLE_GITHUB_ACCESS_TOKEN="<token>";testcafe chrome tests/edit-node-parameter.js
+*/
+
+var EAGLE_GITHUB_ACCESS_TOKEN = process.env.EAGLE_GITHUB_ACCESS_TOKEN;
+var DUMMY_VALUE = "0sdvh5gb8v7r4";
+
+fixture `EAGLE Edit Node Parameter`
+    .page `http://localhost:8888/`
+
+test('Edit node parameter', async t =>{
+    await t
+        // wait for the page to settle down
+        .wait(3000)
+
+        // add a python node to the graph
+        .click("#addPaletteNode6")
+
+        // check that a file node has been created
+        .expect(Selector("#nodeNameValue").value).eql("Python App")
+
+        // edit first parameter in the node inspector
+        .typeText(Selector('#nodeInspectorFieldValue0'), DUMMY_VALUE, {replace: true})
+
+        // check that first parameter was updated
+        .expect(Selector("#nodeInspectorFieldValue0").value).eql(DUMMY_VALUE)
+
+        // end
+        .wait(3000);
+});
