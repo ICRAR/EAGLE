859ce1e09dd483d58d249202291c043bf29529c7
james-strauss-uwa <james.strauss@uwa.edu.au>
2021-05-07 11:51:40 +0800

    Improvements to the load-translate-execute test script

--- tests/load-translate-execute.js
+++ tests/load-translate-execute.js
@@ -1,4 +1,5 @@
 import { Selector } from 'testcafe';
+import page from './page-model';
 
 /*
     run with:
@@ -8,15 +9,18 @@ import { Selector } from 'testcafe';
 
 var EAGLE_GITHUB_ACCESS_TOKEN = process.env.EAGLE_GITHUB_ACCESS_TOKEN;
 
+const TRANSLATOR_URL = "http://localhost:8084/gen_pgt";
 const REPOSITORY_NAME = "ICRAR/EAGLE_test_repo";
 const REPOSITORY_BRANCH = "master";
-const GRAPH_PATH = "SummitIngest_Demo.graph";
+const GRAPH_NAME = "SummitIngest_Demo.graph"; // TODO: at the moment, this onlt works for graphs in the root directory of the repo
 
 fixture `EAGLE Set GitHub Access Token`
     .page `http://localhost:8888/`
 
 test('Set token', async t =>{
     await t
+        // !!!!!!!!!!!!! SETUP
+
         // wait for the page to settle down
         .wait(3000)
 
@@ -25,9 +29,48 @@ test('Set token', async t =>{
         .click("#settings")
 
         // enter the github access token
-        .typeText(Selector('#setting13Value'), EAGLE_GITHUB_ACCESS_TOKEN)
+        .typeText(Selector('#setting13Value'), EAGLE_GITHUB_ACCESS_TOKEN, { replace : true })
+
+        // enter the translator url
+        .typeText(Selector('#setting11Value'), TRANSLATOR_URL, { replace : true })
+
+        // close settings modal
         .click('#settingsModal .modal-footer button')
 
         // end
-        .wait(3000);
+        .wait(3000)
+
+        // !!!!!!!!!!!!! LOAD GRAPH
+
+        // open the repositories tab in the right window
+        .click('#rightWindowModeRepositories')
+
+        // click the required repository name
+        .click('#' + REPOSITORY_NAME.replace('/', '_') + '_' + REPOSITORY_BRANCH)
+
+        // wait for the contents to load
+        .wait(12000)
+
+        // click the graph name
+        .click('#id_' + GRAPH_NAME.replace('.', '_'))
+
+        // wait for the graph to load
+        .wait(8000)
+
+        // !!!!!!!!!!!!! TRANSLATE GRAPH
+
+        // open the translation tab in the right window
+        .click('#rightWindowModeTranslation')
+
+        // open algorithm 1
+        .click('#headingTwo')
+
+        // click the 'Generate PGT' button
+        .click('#alg1PGT');
+
+        // choose the 'OJS' output format
+    await page.selectOption('OJS');
+
+        // wait for the graph to translate
+    await t.wait(8000);
 });
