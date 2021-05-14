aff24a720753212f143faeda4de54d3b4c64c390
james-strauss-uwa <james.strauss@uwa.edu.au>
2021-05-14 15:35:18 +0800

    Send external IP when starting local managers

--- tests/load-translate-execute-2.js
+++ tests/load-translate-execute-2.js
@@ -39,7 +39,7 @@ const startLocalManagers = () => {
     return new Promise((resolve, reject) => {
 
         const data = JSON.stringify({
-            nodes: ['localhost']
+            nodes: [ip]
         });
 
         const options = {
