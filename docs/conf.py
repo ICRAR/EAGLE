30d0f7a65c3dcc2b3b8089ebc6bf8931759bc753
awicenec <awicenec@gmail.com>
2021-07-02 16:22:47 +0800

    Removed additional path

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import subprocess
 import shlex
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
 
