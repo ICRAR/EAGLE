b20cf5419e4043346f3d9d12fc0df17b14b3f96e
Moritz Wicenec <mwicenec@Andreas-MacBook-Pro.local>
2022-02-01 13:20:10 +0800

    multi select header icon buttons and add selected nodes to palette update to work with multiple nodes

--- tests/edit-node-parameter.js
+++ tests/edit-node-parameter.js
@@ -33,7 +33,7 @@ test('Edit node parameter', async t =>{
         .click("#selectPaletteNodePythonApp")
 
         // add selected node to palette
-        .click("#addSelectedNodeToPalette")
+        .click("#addSelectedNodesToPalette")
 
         .wait(1000)
 
