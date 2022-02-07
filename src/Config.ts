45f1adaee4f65d80f62cc18d15b3d5a67af1538d
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-07 13:43:32 +0800

    Load daliuge template palette as 'Raw Components'. Still keep old manually generated palette as legacy

--- src/Config.ts
+++ src/Config.ts
@@ -49,5 +49,7 @@ export class Config {
     static readonly defaultLeftWindowWidth : number = 310;
 
     static readonly DEFAULT_PALETTE_REPOSITORY : string = "ICRAR/EAGLE_test_repo";
-    static readonly DALIUGE_PALETTE_URL : string = "https://raw.githubusercontent.com/ICRAR/EAGLE_test_repo/master/daliuge/daliuge-master.palette";
+
+    static readonly DALIUGE_PALETTE_URL : string  = "https://raw.githubusercontent.com/ICRAR/EAGLE_test_repo/master/daliuge/daliuge-master.palette";
+    static readonly DALIUGE_TEMPLATE_URL : string = "https://raw.githubusercontent.com/ICRAR/EAGLE_test_repo/master/daliuge/daliuge-master-template.palette"
 }
