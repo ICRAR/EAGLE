dc360e482800aef05e0d62411ec0f3264e31de45
Andreas Wicenec <awicenec@gmail.com>
2022-02-10 09:15:48 +0800

    Added option to start the slim image

--- stop_eagle.sh
+++ stop_eagle.sh
@@ -2,11 +2,14 @@ case "$1" in
     "dep")
         docker stop eagle-dep
         exit 1;;
-    "dev")
+   "dev")
         docker stop eagle-dev
         rm prestart.sh
         exit 1;;
+    "slim")
+        docker stop eagle-slim
+        exit 1;;
     *)
-        echo "Usage stop_eagle.sh <dep|dev>"
+        echo "Usage stop_eagle.sh <dep|dev|slim>"
         exit 1;;
 esac
