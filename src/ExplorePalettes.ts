5eefaace1592f5632899506a7803c675a327aaec
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-16 11:20:21 +0800

    Determine list of directories in which the palettes are found

--- src/ExplorePalettes.ts
+++ src/ExplorePalettes.ts
@@ -1,20 +1,42 @@
 import * as ko from "knockout";
 
-import {Eagle} from './Eagle';
 import {PaletteInfo} from './PaletteInfo';
 
 export class ExplorePalettes {
 
     files: ko.ObservableArray<PaletteInfo>;
+    directories: ko.ObservableArray<string>;
 
     constructor(){
         this.files = ko.observableArray([]);
+        this.directories = ko.observableArray([]);
     }
 
     initialise = (palettes: PaletteInfo[]) : void => {
         this.files(palettes);
+        this.directories([]);
 
         // loop through all the palettes to find the directories
+        for (const palette of palettes){
+            const dir = palette.path.split("/")[0];
+            console.log("path", palette.path, "dir", dir);
+
+            // check if directory is already in dorectories array
+            let found = false;
+            for (const directory of this.directories()){
+                if (directory === dir){
+                    found = true;
+                    break;
+                }
+            }
+            if (!found){
+                this.directories.push(dir);
+            }
+
+        }
+
+        // debug
+        console.log("directories", this.directories());
 
         // can we sort non-master palettes by modification date (newest first)
     }
