452474eafab5b25c908c6951a4ff5877a0729016
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-04-28 14:57:49 +0800

    Remove ununsed history() func

--- src/Undo.ts
+++ src/Undo.ts
@@ -137,22 +137,6 @@ export class Undo {
         return result.join(",");
     }
 
-    history : ko.PureComputed<Snapshot[]> = ko.pureComputed(() => {
-        const result : Snapshot[] = [];
-
-        for (let i = Config.UNDO_MEMORY_SIZE - 1 ; i >= 0 ; i--){
-            const index = (i + this.front()) % Config.UNDO_MEMORY_SIZE;
-
-            if (this.memory()[index] === null){
-                break;
-            }
-
-            result.push(this.memory()[index]);
-        }
-
-        return result;
-    }, this);
-
     _loadFromIndex = (index: number, eagle: Eagle) : void => {
         const snapshot : Snapshot = this.memory()[index];
 
