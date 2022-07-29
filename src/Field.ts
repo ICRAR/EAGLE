e0dbc796e4cd9b18c25db0f5d339966e3ad04388
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-07-29 15:31:38 +0800

    Output 'Event' instead of type when outputing to JSON. Handle when loading from JSON too.

--- src/Field.ts
+++ src/Field.ts
@@ -277,7 +277,7 @@ export class Field {
             defaultValue:field.defaultValue(),
             description:field.description(),
             readonly:field.readonly(),
-            type:field.type(),
+            type:field.isEvent() ? "Event" : field.type(),
             precious:field.precious(),
             options:field.options(),
             positional:field.positional()
@@ -292,7 +292,7 @@ export class Field {
             defaultValue:field.defaultValue(),
             description:field.description(),
             readonly:field.readonly(),
-            type:field.type(),
+            type:field.isEvent() ? "Event" : field.type(),
             precious:field.precious(),
             options:field.options(),
             positional: field.positional()
@@ -312,6 +312,7 @@ export class Field {
         let options: string[] = [];
         let positional: boolean = false;
         let fieldType: Eagle.FieldType = Eagle.FieldType.Unknown;
+        let isEvent: Boolean = false;
 
         if (typeof data.id !== 'undefined')
             id = data.id;
@@ -323,8 +324,15 @@ export class Field {
             description = data.description;
         if (typeof data.readonly !== 'undefined')
             readonly = data.readonly;
-        if (typeof data.type !== 'undefined')
-            type = data.type;
+        if (typeof data.type !== 'undefined'){
+            if (data.type === "Event"){
+                isEvent = true;
+                type = Eagle.DataType_Unknown;
+            } else {
+                isEvent = false;
+                type = data.type;
+            }
+        }
         if (typeof data.value !== 'undefined' && data.value !== null)
             value = data.value.toString();
         if (typeof data.defaultValue !== 'undefined' && data.defaultValue !== null)
@@ -338,7 +346,9 @@ export class Field {
         if (typeof data.fieldType !== 'undefined')
             fieldType = data.fieldType;
 
-        return new Field(id, text, name, value, defaultValue, description, readonly, type, precious, options, positional, fieldType);
+        const result = new Field(id, text, name, value, defaultValue, description, readonly, type, precious, options, positional, fieldType);
+        result.setIsEvent(isEvent);
+        return result;
     }
 
     public static sortFunc = (a: Field, b: Field) : number => {
