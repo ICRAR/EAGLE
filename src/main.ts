5d1cac47f770ff6286c3d452911ee38725df3a35
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-10-24 13:47:57 +0800

    Swapped order of palettes

--- src/main.ts
+++ src/main.ts
@@ -107,8 +107,8 @@ $(function(){
     if (Setting.findValue(Utils.OPEN_DEFAULT_PALETTE)){
         eagle.loadPalettes([
             {name:"Builtin Components", filename:Config.DALIUGE_PALETTE_URL, readonly:true},
-            {name:Palette.DYNAMIC_PALETTE_NAME, filename:Config.DALIUGE_TEMPLATE_URL, readonly:true},
-            {name:"Graph Components", filename:window.location.origin + "/static/" + Config.templatePaletteFileName, readonly:true}
+            {name:"Graph Components", filename:window.location.origin + "/static/" + Config.templatePaletteFileName, readonly:true},
+            {name:Palette.DYNAMIC_PALETTE_NAME, filename:Config.DALIUGE_TEMPLATE_URL, readonly:true}
         ], (palettes: Palette[]):void => {
             for (const palette of palettes){
                 if (palette !== null){
