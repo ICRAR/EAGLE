0bad38d6fd8c9848898cc53bdf5def24417a4f09
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-10-27 17:29:45 +0800

    Working unit test for add-remove repository

--- tests/add-remove-repository.js
+++ tests/add-remove-repository.js
@@ -1,5 +1,4 @@
 import { Selector } from 'testcafe';
-import fs from 'fs';
 
 /*
     run with:
@@ -17,6 +16,7 @@ test('Update components', async t =>{
     await t.wait(3000);
 
     // remember number of repos
+    const beforeCount = await Selector('.repositories repository').count;
 
     // click the 'add repository' button
     await t
@@ -27,19 +27,23 @@ test('Update components', async t =>{
     await t
         .typeText(Selector('#gitCustomRepositoryModalRepositoryNameInput'), REPO_NAME, { replace : true, paste : true })
         .typeText(Selector('#gitCustomRepositoryModalRepositoryBranchInput'), REPO_BRANCH, { replace : true, paste : true })
+        .wait(1000)
+        .click(Selector('#gitCustomRepositoryModalAffirmativeButton'))
         .wait(1000);
 
     // check that number of repositories has increased by one
-    await t.expect(obj1.nodeDataArray[2].fields.length).eql(5, {timeout:3000});
+    const afterAddCount = await Selector('.repositories repository').count;
+    await t.expect(afterAddCount).eql(beforeCount + 1);
 
-    // remove the repository 
-    
+    // remove the repository
+    const removeButtonHtmlId = REPO_NAME.replace('/', '_') + "_" + REPO_BRANCH + "-eject";
+    await t
+        .click(Selector('#'+removeButtonHtmlId))
+        .wait(1000)
+        .click(Selector('#confirmModalAffirmativeAnswer'))
+        .wait(1000);
 
     // check that number of repositories has returned to the original value
-    await t.expect(obj1.nodeDataArray[2].fields[0].name).eql("appclass", {timeout:3000});
-
+    const afterRemoveCount = await Selector('.repositories repository').count;
+    await t.expect(afterRemoveCount).eql(beforeCount);
 });
-
-const fetchGraph = (filename) => {
-    graphJSON = fs.readFileSync(filename, 'utf8');
-};
