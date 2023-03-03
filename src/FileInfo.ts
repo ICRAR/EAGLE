843d215921cc8e2acb0d7023a17c84e0b3e22fa9
james-strauss-uwa <james.strauss@uwa.edu.au>
2023-03-03 16:23:10 +0800

    Additional modelData attributes. Added trigger to palette context menu

--- src/FileInfo.ts
+++ src/FileInfo.ts
@@ -364,6 +364,24 @@ export class FileInfo {
         values.push({key:"repoService", value: this._repositoryService(), tooltip:"The service in which this file is stored. Options: " + Utils.enumKeys(Eagle.RepositoryService)});
         values.push({key:"repo", value: this._repositoryName(), tooltip:""});
         values.push({key:"repoBranch", value: this._repositoryBranch(), tooltip:""});
+        values.push({key:"filePath", value: this._path(), tooltip:""});
+        
+        values.push({key:"eagleVersion", value: this._eagleVersion(), tooltip:""});
+        values.push({key:"eagleCommitHash", value: this._eagleCommitHash(), tooltip:""});
+
+        values.push({key:"schemaVersion", value: this._schemaVersion(), tooltip:""});
+        values.push({key:"readonly", value: this._readonly().toString(), tooltip:""});
+
+        values.push({key:"repositoryUrl", value: this._repositoryUrl(), tooltip:""});
+        values.push({key:"commitHash", value: this._commitHash(), tooltip:""});
+        values.push({key:"downloadUrl", value: "<a href='" + this._downloadUrl() + "'>" + this._downloadUrl() + "</a>", tooltip:""});
+        values.push({key:"signature", value: this._signature(), tooltip:""});
+
+        values.push({key:"lastModifiedName", value: this._lastModifiedName(), tooltip:"Name of the user who last modified this file"});
+        values.push({key:"lastModifiedEmail", value: this._lastModifiedEmail(), tooltip:"Email of the user who last modified this file"});
+        values.push({key:"lastModifiedDatetime", value: this._lastModifiedDatetime().toString(), tooltip:"Date and time when this file was last modified"});
+
+        values.push({key:"numLGNodes", value: this._numLGNodes().toString(), tooltip:"Number of nodes within this file"});
 
         return values;
     }
