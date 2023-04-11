612277faa62873e702ff506074eb6efdd5b50a40
MWicenec <wicenecmw@gmail.com>
2023-04-11 10:09:27 +0800

    added the tutorials into the navbar menu

--- src/require-config.ts
+++ src/require-config.ts
@@ -39,7 +39,7 @@ require.config({
         "Setting": "./static/built/Setting",
         "Tutorial": "./static/built/Tutorial",
         "tutorials/quickStart": "./static/built/tutorials/quickStart",
-        "tutorials/graphCreation": "./static/built/tutorials/graphCreation",
+        "tutorials/graphBuilding": "./static/built/tutorials/graphBuilding",
         "SideWindow": "./static/built/SideWindow",
         "KeyboardShortcut": "./static/built/KeyboardShortcut",
         "PaletteInfo": "./static/built/PaletteInfo",
