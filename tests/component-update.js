7418fc2f9b5e98fa470978a45da299c1a84fe807
james-strauss-uwa <james.strauss@uwa.edu.au>
2023-04-19 10:51:38 +0800

    Fixed unit test data and expectations to reflect change to using field text everywhere

--- tests/component-update.js
+++ tests/component-update.js
@@ -67,18 +67,19 @@ test('Update components', async t =>{
 
     // !!!!!!!!!!!!! CHECK FOR EXPECTED INPUT COMPONENTS
     await t.expect(obj1.nodeDataArray[2].fields.length).eql(7, {timeout:3000});
-    await t.expect(obj1.nodeDataArray[2].fields[0].name).eql("Application Class", {timeout:3000});
+    await t.expect(obj1.nodeDataArray[2].fields[0].name).eql("appclass", {timeout:3000});
     await t.expect(obj1.nodeDataArray[2].fields[0].value).eql("dlg.apps.simple.CopyAppBad", {timeout:3000});
 
     // !!!!!!!!!!!!! CHECK FOR CORRECTLY UPDATED OUTPUT COMPONENTS
-    await t.expect(obj2.nodeDataArray[2].fields.length).eql(10, {timeout:3000});
-    await t.expect(obj2.nodeDataArray[2].fields[0].name).eql("Application Class", {timeout:3000});
+    await t.expect(obj2.nodeDataArray[2].fields.length).eql(11, {timeout:3000});
+    await t.expect(obj2.nodeDataArray[2].fields[0].name).eql("appclass", {timeout:3000});
     await t.expect(obj2.nodeDataArray[2].fields[0].value).eql("dlg.apps.simple.CopyApp", {timeout:3000});
     await t.expect(obj2.nodeDataArray[2].fields[5].name).eql("hello", {timeout:3000});
     await t.expect(obj2.nodeDataArray[2].fields[6].name).eql("hello", {timeout:3000});
-    await t.expect(obj2.nodeDataArray[2].fields[7].name).eql("buffer size", {timeout:3000});
-    await t.expect(obj2.nodeDataArray[2].fields[8].name).eql("Number of tries", {timeout:3000});
-    await t.expect(obj2.nodeDataArray[2].fields[9].name).eql("dummy", {timeout:3000});
+    await t.expect(obj2.nodeDataArray[2].fields[7].name).eql("bufsize", {timeout:3000});
+    await t.expect(obj2.nodeDataArray[2].fields[8].name).eql("n_tries", {timeout:3000});
+    await t.expect(obj2.nodeDataArray[2].fields[9].name).eql("dummy_in", {timeout:3000});
+    await t.expect(obj2.nodeDataArray[2].fields[10].name).eql("dummy_out", {timeout:3000});
 });
 
 const fetchGraph = (filename) => {
