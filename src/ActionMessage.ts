a0e7df6efe4efc38b4fc17038f2e06054d5ad387
james-strauss-uwa <james.strauss@uwa.edu.au>
2023-05-02 13:27:34 +0800

    Add level to the ActionMessage

--- src/ActionMessage.ts
+++ src/ActionMessage.ts
@@ -1,23 +1,38 @@
 export class ActionMessage {
+    level: ActionMessage.Level;
     message: string;
     show: () => void;
     fix: () => void;
-    fixDescription: string
+    fixDescription: string;
 
-    constructor (message: string, show: () => void, fix: () => void, fixDescription: string){
+    constructor (level: ActionMessage.Level, message: string, show: () => void, fix: () => void, fixDescription: string){
+        this.level = level;
         this.message = message;
         this.show = show;
         this.fix = fix;
         this.fixDescription = fixDescription;
     }
 
-    static Message(message: string): ActionMessage {
-        return new ActionMessage(message, null, null, "");
+    static Message(level: ActionMessage.Level, message: string): ActionMessage {
+        return new ActionMessage(level, message, null, null, "");
     }
-    static Show(message: string, show: () => void): ActionMessage {
-        return new ActionMessage(message, show, null, "");
+    static Show(level: ActionMessage.Level, message: string, show: () => void): ActionMessage {
+        return new ActionMessage(level, message, show, null, "");
     }
-    static Fix(message: string, show: () => void, fix: () => void, fixDescription: string): ActionMessage {
-        return new ActionMessage(message, show, fix, fixDescription);
+    static Fix(level: ActionMessage.Level, message: string, show: () => void, fix: () => void, fixDescription: string): ActionMessage {
+        return new ActionMessage(level, message, show, fix, fixDescription);
+    }
+}
+
+export namespace ActionMessage {
+    export enum Level {
+        Primary = "Primary",
+        Secondary = "Secondary",
+        Success = "Success",
+        Danger = "Danger",
+        Warning = "Warning",
+        Info = "Info",
+        Light = "Light",
+        Dark ="Dark",
     }
 }
