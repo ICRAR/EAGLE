92f30a63ff847539ba8a3ae9beaa26b9f82c2c9e
MWicenec <wicenecmw@gmail.com>
2024-06-27 15:46:01 +0800

    added another check for un neccessary loop aware edges

--- src/Edge.ts
+++ src/Edge.ts
@@ -424,7 +424,7 @@ export class Edge {
             }
         }
 
-        if(isSibling && loopAware){
+        if(isSibling && loopAware || destPortIsEmbeddedAppOfSibling && loopAware || srcPortIsEmbeddedAppOfSibling && loopAware || sourceNode.getEmbedKey() === destinationNode.getParentKey() && loopAware || destinationNode.getEmbedKey() === sourceNode.getParentKey() && loopAware){
             const x = Errors.ShowFix("An edge between two siblings should not be loop aware", function(){Utils.showEdge(eagle, edgeId);}, function(){Utils.fixDisableEdgeLoopAware(eagle, edgeId);}, "Disable loop aware on the edge.");
             Edge.isValidLog(edgeId, Eagle.LinkValid.Invalid, x, showNotification, showConsole, errorsWarnings);
         }
