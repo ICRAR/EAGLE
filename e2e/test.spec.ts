81f6453834ead9b8d471d3dd1d1512720f3a7e46
MWicenec <wicenecmw@gmail.com>
2024-08-01 17:18:00 +0800

    experimenting with new tests, still struggling with this particular modal

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -8,60 +8,22 @@ test('Eagle has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/EAGLE/);
 
-  //open settings modal
-  await page.locator('#settings').click()
+  const REPO_NAME = "ICRAR/daliuge";
+  const REPO_BRANCH = "yan-812-2";
+  const repoExists :boolean = await page.locator('#ICRAR_daliuge_yan-812-2').count() === 1
 
-  //enable expert mode
-  const uiModeSelect = await page.getByPlaceholder('uiMode')
-  uiModeSelect.selectOption({value:'Expert'})
+  //just making sure the repo doesnt exist for some reason, if it does, remove it
+  if(repoExists){
+    await page.locator('.repoConatiner').filter({has:page.locator('#ICRAR_daliuge_yan-812-2')}).getByText('eject').click()
+  }
 
-  //close settings modal (wait is needed, bootstrap is not ready to close the modal again that quickly)
-  await page.waitForTimeout(500);
-  await page.getByRole('button', { name: 'OK' }).click()
+  await page.getByRole('button',{name:'Add Repository'}).click()
+  await page.waitForTimeout(1000);//wait needed to allow the input fields to be ready to detect changes to their values
 
-  //right click the hellow world app in the palette
-  await page.locator('#palette_0_CopyApp').click({
-    button: 'right'
-  });
+  await page.locator('input#gitCustomRepositoryModalRepositoryNameInput').fill(REPO_NAME)
+  await page.locator('input#gitCustomRepositoryModalRepositoryBranchInput').fill(REPO_BRANCH)
+  await page.waitForTimeout(1000);//wait needed to allow the input fields to be ready to detect changes to their values
 
-  //click menu item add to another palette
-  await page.getByText('Add to another palette').click();
-
-  //select the input field to give the new palette a name
-  await page.getByRole('textbox', { name: 'Custom Port Name' }).click();
-  //write test
-  await page.getByRole('textbox', { name: 'Custom Port Name' }).fill('test');
-  //timeout for bootstrap animation and confirm the modal
-  await page.waitForTimeout(500);
-  await page.getByRole('button', { name: 'OK' }).click();
-
-  //add a helloworld app to the graph by clicking it's icon
-  await page.locator('#addPaletteNodeHelloWorldApp').click();
-  //agree to create a new graph with it's auto-generated name
-  await page.waitForTimeout(500);
-  await page.getByRole('button', { name: 'OK' }).click();
-
-  //scroll the file node into view in the palette
-  await page.locator('#palette_1_File').scrollIntoViewIfNeeded()
-  await page.dragAndDrop( '#palette_1_File' ,'#graphArea', {targetPosition:{x:500,y:500}})
-
-  //center the graph
-  await page.getByRole('button', { name: 'filter_center_focus' }).click();
-
-  await page.waitForTimeout(500);
-
-  //drag an edge from helloWorldApp -> File
-  await page.dragAndDrop('#HelloWorldApp .outputPort', '#File .inputPort',{sourcePosition:{x:2,y:2},targetPosition:{x:2,y:2}})
-
-
-  //click on the input port of the file to open the parameter table modal and highlight the port
-  await page.locator('.inputPort').click();
-  //rename the port
-  await page.locator('.highlighted .tableFieldDisplayName').fill('testInput');
-
-  //wait for bootstrap modal then close
-  await page.waitForTimeout(500);
-  await page.locator('#parameterTableModal').getByRole('button', { name: 'Close' }).click();
-
-  await page.close();
+  await page.locator('button#gitCustomRepositoryModalAffirmativeButton').click()
+  // await page.close();
 });
