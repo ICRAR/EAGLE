5e99eef7a348d6a426b7990ad368c5f1de4909f8
MWicenec <wicenecmw@gmail.com>
2024-08-15 12:05:32 +0800

    simplified the status bar array

--- src/StatusEntry.ts
+++ src/StatusEntry.ts
@@ -6,35 +6,43 @@ import { Utils } from './Utils';
 export class StatusEntry {
     action:string;
     message:string;
-    shortcut:string;
     visibility:ko.Observable<boolean>;
 
-    constructor(action:string,message:string,shortcut:string,visibility:boolean){
-        this.action = action
+    constructor(action:string,message:string,visibility:boolean){
+        this.action = action //write an action of get a shortcut
         this.message = message
-        this.shortcut = shortcut
         this.visibility = ko.observable(visibility)
+    }
+
+    static constructIsSelected():boolean {
+        if(Eagle.getInstance().selectedNode() != null){
+            return Eagle.getInstance().selectedNode().isConstruct()
+        }
+
+        return false
     }
 
     static getStatusEntries() : StatusEntry[] {
         return [
             //nothing is selected
-            new StatusEntry('Right Click ',' on the empty canvas to add nodes.','', Eagle.getInstance().selectedObjects().length === 0),
-            new StatusEntry('',' search and run functions.',Utils.getKeyboardShortcutTextByKey('quick_action', true), Eagle.getInstance().selectedObjects().length === 0),
-            new StatusEntry('Click ',' on a node in the graph to select it.','', Eagle.getInstance().selectedObjects().length === 0),
+            new StatusEntry('Right Click ',' on the empty canvas to add nodes.', Eagle.getInstance().selectedObjects().length === 0),
+            new StatusEntry(Utils.getKeyboardShortcutTextByKey('quick_action', true),' search and run functions.', Eagle.getInstance().selectedObjects().length === 0),
             //a graph is created or loaded
-            new StatusEntry('',' access graph config.',Utils.getKeyboardShortcutTextByKey('open_key_parameter_table_modal', true), Eagle.getInstance().logicalGraph().fileInfo().name != "" && Eagle.getInstance().selectedObjects().length === 0),
+            new StatusEntry(Utils.getKeyboardShortcutTextByKey('open_key_parameter_table_modal', true),' access graph config.', Eagle.getInstance().logicalGraph().fileInfo().name != "" && Eagle.getInstance().selectedObjects().length === 0),
             //No Graph loaded or created
-            new StatusEntry('',' new graph.',Utils.getKeyboardShortcutTextByKey('new_graph', true), Eagle.getInstance().logicalGraph().fileInfo().name === ""),
+            new StatusEntry(Utils.getKeyboardShortcutTextByKey('new_graph', true),' new graph.', Eagle.getInstance().logicalGraph().fileInfo().name === ""),
             //something is selected
-            new StatusEntry('Right Click ',' on Objects in the graph to see more options.','', Eagle.getInstance().selectedObjects().length > 0),
+            new StatusEntry('Right Click ',' on Objects in the graph to see more options.', Eagle.getInstance().selectedObjects().length > 0),
+            new StatusEntry(Utils.getKeyboardShortcutTextByKey('duplicate_selection', true),' duplicate selected objects.', Eagle.getInstance().selectedObjects().length > 0),
+            new StatusEntry(Utils.getKeyboardShortcutTextByKey('delete_selection', true),' delete selected objects.', Eagle.getInstance().selectedObjects().length > 0),
             //node is selected
-            new StatusEntry('',' delete selected node.',Utils.getKeyboardShortcutTextByKey('delete_selection', true), Eagle.getInstance().selectedNode() != null),
-            new StatusEntry('',' open fields table.',Utils.getKeyboardShortcutTextByKey('open_component_parameter_table_modal', true), Eagle.getInstance().selectedNode() != null && Setting.findValue(Setting.ALLOW_GRAPH_EDITING)),
-            //edge is selected
-            new StatusEntry('',' delete selected edge.',Utils.getKeyboardShortcutTextByKey('delete_selection', true), Eagle.getInstance().selectedEdge() != null),
+            new StatusEntry(Utils.getKeyboardShortcutTextByKey('open_component_parameter_table_modal', true),' open fields table.', Eagle.getInstance().selectedNode() != null && Setting.findValue(Setting.ALLOW_GRAPH_EDITING)),
             //more than one thing is selected
-            new StatusEntry('',' delete selected objects.',Utils.getKeyboardShortcutTextByKey('delete_selection', true), Eagle.getInstance().selectedObjects().length >1),
+            new StatusEntry('Shift + Drag',' Box select multiple objects.', Eagle.getInstance().selectedObjects().length >1),
+            new StatusEntry('Shift + Ctrl + Drag',' Box deselect multiple objects.', Eagle.getInstance().selectedObjects().length >1),
+            //construct is selected
+            new StatusEntry('Alt + click',' Select a construct and its children.', this.constructIsSelected()),
+
         ];
     }
 }
