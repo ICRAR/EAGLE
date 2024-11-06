ebaf0e2610ddc915536c20608b2e99c7fd5ce0ea
james-strauss-uwa <james.strauss@uwa.edu.au>
2024-11-06 12:37:40 +0800

    Added a comment to explain the special case in getNodeLockedState for when we're in the Graph Configuration Attributes Table mode. It doesn't make sense to be able to modify the name of the attribute.

--- src/ParameterTable.ts
+++ src/ParameterTable.ts
@@ -204,6 +204,8 @@ export class ParameterTable {
     // TODO: move to Eagle.ts?
     //       doesn't seem to depend on any ParameterTable state, only Eagle state
     static getNodeLockedState = (field:Field) : boolean => {
+        // this handles a special case where EAGLE is displaying the "Graph Configuration Attributes Table"
+        // all the fields shown in that table should be locked (readonly)
         if (Setting.find(Setting.BOTTOM_WINDOW_MODE).value() === Eagle.BottomWindowMode.GraphConfigAttributesTable){
             return true;
         }
