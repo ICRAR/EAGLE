329741148507787560e8dca2ecea96b3043c6a38
M-Wicenec <wicenecmw@gmail.com>
2025-02-18 14:50:46 +0800

    testing playwright functions wip c

---
+++ e2e/recordingSimpleTutorial.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect,chromium } from '@playwright/test';
+
+test.use({ 
+  video: {
+    mode: 'on',
+    size: { width: 1920, height: 1080 }
+  },
+  viewport: { width: 1920, height: 1080 },
+  launchOptions:{
+    slowMo: 700
+  }
+});
+
+test('recording simple tutorial', async () => {
+
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  // const testing123 = page.evaluate(() => {
+  //   function testing() {
+  //     alert('testing')
+  //   } 
+  // })
+
+  await page.goto('http://localhost:8888/');
+  await page.getByRole('button', { name: 'Graph' }).hover();
+  await page.getByRole('button', { name: 'Graph' }).click();
+  await page.getByText('New Create New Graph [ N ]').hover();
+  await page.getByRole('link', { name: 'Create New Graph [ N ]' }).hover();
+  await page.getByRole('link', { name: 'Create New Graph [ N ]' }).click();
+  await page.locator('#inputModalInput').press('ControlOrMeta+a');
+  await page.locator('#inputModalInput').pressSequentially('myNewGraph');
+  await page.locator('#inputModalInput').press('Enter');
+
+  await page.pause()
+  // await page.evaluate(() => {
+  //   testing123  
+  // })
+
+  // await page.close();
+});
