3bce21143208bb98e301cb9ca6d24d51b5bda839
MWicenec <wicenecmw@gmail.com>
2025-03-12 10:43:20 +0800

    fine adjustments to the video

--- e2e/recordingSimpleTutorial.spec.ts
+++ e2e/recordingSimpleTutorial.spec.ts
@@ -60,11 +60,10 @@ test('Creating Hellow World Example', async ({ page }) => {
   await moveMouseCursor(page, clickTarget)
   await clickTarget.click();
 
-  //move mouse to graph info
+  //move mouse to graph info explain then click
   clickTarget = await page.getByRole('button', { name: 'info' })
   await moveMouseCursor(page, clickTarget)
-  await explainElement(page, clickTarget, 'down', 'This is where you can access the graph info modal. It gives access to information like graph author, date last edited and graph descriptions.', 6000)
-  
+  await explainElement(page, clickTarget, 'down', 'This is where you can access the graph info modal. It gives access to information like graph author, date last edited and graph descriptions.', 7000)
   await clickTarget.click();
   
   //move mouse to graph description, show message and enter a description
@@ -72,7 +71,8 @@ test('Creating Hellow World Example', async ({ page }) => {
   await moveMouseCursor(page, clickTarget)
   await explainElement(page, clickTarget, 'down', 'Lets enter a description for our graph.', 3000)
   await clickTarget.click();
-  await clickTarget.pressSequentially('A graph saving the output of a HelloWorldApp to disk.');
+  await clickTarget.pressSequentially('A graph saving the output of a HelloWorldApp to disk.');    
+  await page.waitForTimeout(500); // small wait to let the viewer see what happened
 
   //click ok to close the modal
   clickTarget = await page.getByRole('button', { name: 'OK' })
@@ -88,6 +88,7 @@ test('Creating Hellow World Example', async ({ page }) => {
   clickTarget = await page.locator('#addPaletteNodeHelloWorldApp')
   await moveMouseCursor(page, clickTarget)
   await clickTarget.hover();
+  await page.waitForTimeout(1000); // small wait to let the viewer see what happened
   await explainElement(page, clickTarget, 'right', 'Hover over the icon of a palette component to find out information about it. Then click it to add it to the graph.', 6000)
   await clickTarget.click();
 
@@ -101,7 +102,7 @@ test('Creating Hellow World Example', async ({ page }) => {
   //explain the right click menu and search then add the file node by pressing enter
   clickTarget = await page.locator('#customContextMenu .searchBarContainer')
   await moveMouseCursor(page, clickTarget)
-  await explainElement(page, clickTarget, 'up', 'You can right click on many elements in eagle to get extra options. If you right click on an empty part of the canvas, you can quickly add a node to the graph.', 6000)
+  await explainElement(page, clickTarget, 'up', 'You can right click on many elements in eagle to get extra options. If you right click on an empty part of the canvas, you can quickly add a node to the graph.', 7500)
   await explainElement(page, clickTarget, 'up', 'We will search for "file" and press "enter" to quickly add a file node to our graph.', 4000)
   //search for file then press enter to add the file node to the graph
   await clickTarget.pressSequentially('file')
@@ -109,17 +110,18 @@ test('Creating Hellow World Example', async ({ page }) => {
 
   clickTarget = await page.locator('#logicalGraph .node').first().locator('.body')
   await moveMouseCursor(page, clickTarget)
-  await explainElement(page, clickTarget, 'down',"Nodes can be selected to view and edit their parameters. A node's name can be edited by clicking on it in the graph.", 5000)
+  await explainElement(page, clickTarget, 'down',"Nodes can be selected to view and edit their parameters. A node's name can be edited by clicking on it in the graph.", 6000)
   await clickTarget.click()
 
   //explaination for the inspector
   clickTarget = await page.locator('#inspector .container')
+  await moveMouseCursor(page, clickTarget)
   await explainElement(page, clickTarget, 'up',"Basic information and some simple actions for the selected element can be seen here.", 4000)
 
   //explain then click on the parameter table button
   clickTarget = await page.locator('#openNodeParamsTable')
   await moveMouseCursor(page, clickTarget)
-  await explainElement(page, clickTarget, 'up',"The node fields table is for more advanced aditing of the node. You can access it here.", 4000)
+  await explainElement(page, clickTarget, 'up',"The node fields table is for more advanced editing of the node. You can access it here.", 4000)
   await clickTarget.click()
 
   //change the name of who we are greeting
@@ -127,7 +129,8 @@ test('Creating Hellow World Example', async ({ page }) => {
   await moveMouseCursor(page, clickTarget)
   await clickTarget.click();
   await clickTarget.press('ControlOrMeta+a');
-  await clickTarget.pressSequentially('Felicia');
+  await clickTarget.pressSequentially('Felicia');  
+  await page.waitForTimeout(500); // small wait to let the viewer see what happened
 
   //close the bottom window
   clickTarget = await page.locator('.closeBottomWindowBtn')
@@ -138,12 +141,13 @@ test('Creating Hellow World Example', async ({ page }) => {
   const outputPort = await page.locator('#HelloWorldApp .outputPort')
   const inputPort = await page.locator('#File .inputPort')
   await explainElement(page, outputPort, 'down', 'This is the output port of the hello world app.',3000)
-  await explainElement(page, inputPort, 'down', 'And this is the input port of the File node. Connecting these two nodes means we are saving the output of hello world to disk.',6000)
+  await explainElement(page, inputPort, 'down', 'And this is the input port of the File node. Connecting these two nodes means we are saving the output of the hello world app to disk.',6000)
   await moveMouseCursor(page, outputPort)
   await moveMouseCursor(page, inputPort)
   await page.dragAndDrop('#HelloWorldApp .outputPort', '#File .inputPort',{sourcePosition:{x:2,y:2},targetPosition:{x:2,y:2}})
 
-  await textNotification(page, 'Tutorial: ', 'Finished graph creation tutorial.', 2500);
+  //end notification
+  await textNotification(page, 'Tutorial: ', 'Finished graph creation tutorial.', 3000);
 
   await page.close();
 });
