761e88aec815ccc8cce24e57abb5da495030cf0a
james-strauss-uwa <james.strauss@uwa.edu.au>
2025-06-05 13:38:38 +0800

    Just use one graph in test

--- e2e/loadSaveJsonMatch.spec.ts
+++ e2e/loadSaveJsonMatch.spec.ts
@@ -3,64 +3,56 @@ import fs from 'fs';
 import https from 'https';
 import path from 'path';
 
-
-const GRAPHS = [
-  // graph_patterns
-  //"https://raw.githubusercontent.com/ICRAR/EAGLE-graph-repo/refs/heads/master/graph_patterns/LoopWithBranch.graph",
-  //"https://raw.githubusercontent.com/ICRAR/EAGLE-graph-repo/refs/heads/master/graph_patterns/genericScatter.graph"
-
-  "data/LoopWithBranch.graph"
-]
+const INPUT_GRAPH_LOCATION: string = "data/LoopWithBranch.graph";
+//const INPUT_GRAPH_LOCATION: string = "https://raw.githubusercontent.com/ICRAR/EAGLE-graph-repo/refs/heads/master/graph_patterns/LoopWithBranch.graph";
 
 let graphJSON: string = "input";
 
 test('Load/Save JSON Match', async ({ page }) => {
-  for (const graph of GRAPHS){
-    //await fetchGraph(graph);
-    await readGraph(graph);
+  //await fetchGraph(graph);
+  await readGraph(INPUT_GRAPH_LOCATION);
 
-    await page.goto('http://localhost:8888/?tutorial=none');
+  await page.goto('http://localhost:8888/?tutorial=none');
 
-    // navigate menus and click 
-    await page.locator('#navbarDropdownGraph').click();
-    await page.locator('#navbarDropdownGraphNew').hover();
-    await page.locator('#createNewGraphFromJson').click();
+  // navigate menus and click 
+  await page.locator('#navbarDropdownGraph').click();
+  await page.locator('#navbarDropdownGraphNew').hover();
+  await page.locator('#createNewGraphFromJson').click();
 
-    // short wait
-    await page.waitForTimeout(500);
+  // short wait
+  await page.waitForTimeout(500);
 
-    // enter the graph JSON into the modal
-    await page.evaluate(setEditorContent, graphJSON);
+  // enter the graph JSON into the modal
+  await page.evaluate(setEditorContent, graphJSON);
 
-    // click the OK button on the modal
-    await page.locator('#inputCodeModal .modal-footer button.btn-primary').click();
+  // click the OK button on the modal
+  await page.locator('#inputCodeModal .modal-footer button.btn-primary').click();
 
-    // wait for the 'graph load success' notification to be shown
-    await page.locator('div[data-notify="container"]').waitFor({state: 'attached'});
+  // wait for the 'graph load success' notification to be shown
+  await page.locator('div[data-notify="container"]').waitFor({state: 'attached'});
 
-    // dismiss notification
-    await page.locator('button[data-notify="dismiss"]').click();
+  // dismiss notification
+  await page.locator('button[data-notify="dismiss"]').click();
 
-    // wait for the 'graph load success' notification to be dismissed
-    await page.locator('div[data-notify="container"]').waitFor({state: 'detached'});
+  // wait for the 'graph load success' notification to be dismissed
+  await page.locator('div[data-notify="container"]').waitFor({state: 'detached'});
 
-    // click 'display as JSON' from the 'Graph' menu
-    await page.locator('#navbarDropdownGraph').click();
-    await page.locator('#displayGraphAsJson').click();
+  // click 'display as JSON' from the 'Graph' menu
+  await page.locator('#navbarDropdownGraph').click();
+  await page.locator('#displayGraphAsJson').click();
 
-    // get JSON from modal
-    const outputJSON: string = await page.evaluate(getEditorContent);
-    
-    const obj1 = JSON.parse(graphJSON);
-    const obj2 = JSON.parse(outputJSON);
+  // get JSON from modal
+  const outputJSON: string = await page.evaluate(getEditorContent);
+  
+  const obj1 = JSON.parse(graphJSON);
+  const obj2 = JSON.parse(outputJSON);
 
-    const result0 = compareObj(obj1, obj2);
-    const result1 = compareObj(obj2, obj1);
+  const result0 = compareObj(obj1, obj2);
+  const result1 = compareObj(obj2, obj1);
 
-    // !!!!!!!!!!!!! CHECK FOR MATCH
-    await expect(JSON.stringify(result0)).toBe("{}");
-    await expect(JSON.stringify(result1)).toBe("{}");
-  }
+  // !!!!!!!!!!!!! CHECK FOR MATCH
+  await expect(JSON.stringify(result0)).toBe("{}");
+  await expect(JSON.stringify(result1)).toBe("{}");
 
   //closing the browser
   await page.close();
