1bcd5eb750e6aa40844102ef49278744a76fc349
MWicenec <wicenecmw@gmail.com>
2025-06-11 10:40:17 +0800

    rewrote quick search algorithm again

--- src/QuickActions.ts
+++ src/QuickActions.ts
@@ -47,32 +47,53 @@ export class QuickActions {
 
     static results : ko.PureComputed<KeyboardShortcut[]> = ko.pureComputed(() => {
         const searchTerm: string = QuickActions.searchTerm().toLocaleLowerCase();
-        const resultsList: KeyboardShortcut[] = [];
+        let resultsList: KeyboardShortcut[] = [];
 
-        const results: Results = {perfectNameMatch:[], nameStartsWith:[], perfectTagMatch:[], tagStartsWith:[], nameIncludes:[], namePartial:[], tagIncludes:[], tagPartial:[]};
+        const results: {keyboardShortcut:KeyboardShortcut, priority:number}[] = [];
+
+        // const results: Results = {perfectNameMatch:[], nameStartsWith:[], perfectTagMatch:[], tagStartsWith:[], nameIncludes:[], namePartial:[], tagIncludes:[], tagPartial:[]};
 
         if(searchTerm != ''){
             // processing the keyboard shortcuts array
             KeyboardShortcut.shortcuts.forEach(function(shortcut:KeyboardShortcut){
-                const priority: Priority = QuickActions.checkMatch(shortcut, searchTerm);
-                if(priority !== null){
+                const priority: number = QuickActions.checkMatch(shortcut, searchTerm);
+                if(priority !== 0){
                     //pushing the results in order of priority
-                    QuickActions.pushResultUsingPriority(priority, shortcut, results);
+                    // QuickActions.pushResultUsingPriority(priority, shortcut, resultsList);
+                    results.push({keyboardShortcut:shortcut, priority: priority})
                 }
+                // const priority: Priority = QuickActions.checkMatch(shortcut, searchTerm);
+                // if(priority !== null){
+                //     //pushing the results in order of priority
+                //     QuickActions.pushResultUsingPriority(priority, shortcut, results);
+                // }
             })
 
             // processing the quick start array
             KeyboardShortcut.quickActions.forEach(function(shortcut:KeyboardShortcut){
                 const priority: Priority = QuickActions.checkMatch(shortcut, searchTerm);
-                if(priority !== null){
+                if(priority !== 0){
+                    results.push({keyboardShortcut:shortcut, priority: priority})
                     //pushing the results in order of priority
-                    QuickActions.pushResultUsingPriority(priority, shortcut, results);
+                    // QuickActions.pushResultUsingPriority(priority, shortcut, results);
                 }
             })
 
+            //sort the array by priority
+            results.sort((a, b) =>b.priority - a.priority);
+            const test = results.map(result => (result.keyboardShortcut.text, result.priority))
+            console.log('sorted' , test)
+// let newArray = driver.map(pts => (pts["race_pts"]))
+            resultsList = results.map(result => (result.keyboardShortcut))
+            console.log(resultsList)
+            //TODO need to sort the results array according to their priority score 
+
+            //TODO need to then push the keyboard shortcuts from the results array to the resultsList excluding the priority score
+            
+
             //adding the contents of each of the priority arrays into the results array, in order of priority
             //the ... means we are appending only the array's entries not the array itself
-            resultsList.push(...results.perfectNameMatch, ...results.nameStartsWith, ...results.perfectTagMatch,...results.tagStartsWith,...results.nameIncludes,...results.namePartial,...results.tagIncludes,...results.tagPartial)
+            // resultsList.push(...results.perfectNameMatch, ...results.nameStartsWith, ...results.perfectTagMatch,...results.tagStartsWith,...results.nameIncludes,...results.namePartial,...results.tagIncludes,...results.tagPartial)
         }
 
         // when the search result list changes we reset the selected result
@@ -81,108 +102,168 @@ export class QuickActions {
         return resultsList
     }, this);
 
-    static pushResultUsingPriority(priority: Priority, shortcut: KeyboardShortcut, results: Results) : void {
-        if(priority === Priority.PerfectNameMatch){
-            results.perfectNameMatch.push(shortcut)
-        }else if(priority === Priority.NameStartsWith){
-            results.nameStartsWith.push(shortcut)
-        }else if(priority === Priority.PerfectTagMatch){
-            results.perfectTagMatch.push(shortcut)
-        }else if(priority === Priority.TagStartsWith){
-            results.tagStartsWith.push(shortcut)
-        }else if(priority === Priority.NameIncludes){
-            results.nameIncludes.push(shortcut)
-        }else if(priority === Priority.NamePartial){
-            results.namePartial.push(shortcut)
-        }else if(priority === Priority.TagIncludes){
-            results.tagIncludes.push(shortcut)
-        }else if(priority === Priority.TagPartial){
-            results.tagPartial.push(shortcut)
-        }
-    }
+    // static pushResultUsingPriority(priority: Priority, shortcut: KeyboardShortcut, results: Results) : void {
+    //     if(priority === Priority.PerfectNameMatch){
+    //         results.perfectNameMatch.push(shortcut)
+    //     }else if(priority === Priority.NameStartsWith){
+    //         results.nameStartsWith.push(shortcut)
+    //     }else if(priority === Priority.PerfectTagMatch){
+    //         results.perfectTagMatch.push(shortcut)
+    //     }else if(priority === Priority.TagStartsWith){
+    //         results.tagStartsWith.push(shortcut)
+    //     }else if(priority === Priority.NameIncludes){
+    //         results.nameIncludes.push(shortcut)
+    //     }else if(priority === Priority.NamePartial){
+    //         results.namePartial.push(shortcut)
+    //     }else if(priority === Priority.TagIncludes){
+    //         results.tagIncludes.push(shortcut)
+    //     }else if(priority === Priority.TagPartial){
+    //         results.tagPartial.push(shortcut)
+    //     }
+    // }
 
-    static checkMatch(shortcut: KeyboardShortcut, searchTerm: string): Priority | null {
-        // let match = false
+    static checkMatch(shortcut: KeyboardShortcut, searchTerm: string): number {
+        const perfectWordMatchPoints = 10000
+        const startsWithPoints = 100
+        const wordMatchPoints = 50
+        const wordMatchInSequencePoints = 2
+        const partialWordMatchPoints = 10
+        const perfectTagMatchPoints = 40
+        const tagMatchPoints = 30
+        const partialTagMatchPoints = 7
 
-        // check for match against shortcut text
-        // if(shortcut.text.toLocaleLowerCase().includes(searchTerm)){
-        //     match = true
-        // }
+        let shortcutMatchScore = 0
 
-        // check for match against shortcut tags
-        // shortcut.tags.forEach(function(tag){
-        //     if(tag.toLocaleLowerCase().includes(searchTerm)){
-        //         match = true
-        //     }
-        // })
-
-        // abort if no match found
-        // if (!match){
-        //     return null;
-        // }
-
-
-        //check for perfect match to with function name
+        //perfect match! no point to continue
         if (shortcut.text.toLocaleLowerCase() === searchTerm.toLocaleLowerCase()){
-            return Priority.PerfectNameMatch
+            return perfectWordMatchPoints
         }
 
         //check if function name starts with the search term
         if (shortcut.text.toLocaleLowerCase().startsWith(searchTerm.toLocaleLowerCase())){
-            return Priority.NameStartsWith
+            shortcutMatchScore+=startsWithPoints
         }
 
-        //check for simple tag matches
-        let perfectTagMatch = false
-        let TagStartsWith = false
         for(const tag of shortcut.tags){
             // check for perfect tag matchs
             if (tag.toLocaleLowerCase() === searchTerm.toLocaleLowerCase()){
-                perfectTagMatch = true;
-                break
-            }
-
-            // check if the tag starts with the search term
-            if (tag.startsWith(searchTerm.toLocaleLowerCase())){
-                TagStartsWith = true;
+                shortcutMatchScore += perfectTagMatchPoints
             }
         }
-        if(perfectTagMatch){
-            return Priority.PerfectTagMatch
-        }else if(TagStartsWith){
-            return Priority.TagStartsWith
-        }
 
-
-        // the rest can stay
 
         // split both the shortcut text and search term into individual words
         const shortcutWords: string[] = shortcut.text.split(' ');
         const searchWords: string[] = searchTerm.split(' ').filter(Boolean);
 
-        for (const searchWord of searchWords){
-            
+        let searchWordStillMatches = true // keeping track of how many of the words from the search word entry match the keyboardshortcut name 
+
+        for (let searchWordIndex = 0; searchWordIndex < searchWords.length ; searchWordIndex++){
+            const searchWord = searchWords[searchWordIndex]
+
             // check for match against words in shortcut
-            for(const shortcutWord of shortcutWords){
+            for(let shortcutWordIndex = 0; shortcutWordIndex < shortcutWords.length ; shortcutWordIndex++){
+                const shortcutWord = shortcutWords[shortcutWordIndex]
+
                 if(shortcutWord.toLocaleLowerCase() === searchWord.toLocaleLowerCase()){
-                    return Priority.NameIncludes;
-                }else if(shortcutWord.toLocaleLowerCase().startsWith(searchWord.toLocaleLowerCase())){
-                    return Priority.NamePartial;
+                    shortcutMatchScore += wordMatchPoints
+
+                    if(searchWordStillMatches && searchWordIndex === shortcutWordIndex){
+                        console.log('words match in sequence points added', shortcutWords)
+                        shortcutMatchScore += wordMatchInSequencePoints
+                    }else{
+                        searchWordStillMatches = false;
+                    }
+
+                }else if(shortcutWord.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase())){
+                    shortcutMatchScore += partialWordMatchPoints
+
+                    if(searchWordStillMatches && searchWordIndex === shortcutWordIndex){
+                        console.log('words match in sequence points added', shortcutWords)
+                        shortcutMatchScore += wordMatchInSequencePoints
+                    }else{
+                        searchWordStillMatches = false;
+                    }
+                }else{
+                    searchWordStillMatches = false
                 }
             }
 
             // check for match against shortcut tags
             for(const tag of shortcut.tags){
                 if(searchTerm.toLocaleLowerCase() === tag.toLocaleLowerCase()){
-                    return Priority.TagIncludes;
-                }else if(tag.toLocaleLowerCase().startsWith(searchWord.toLocaleLowerCase())){
-                    return Priority.TagPartial;
+                    shortcutMatchScore += tagMatchPoints
+                }else if(tag.toLocaleLowerCase().includes(searchWord.toLocaleLowerCase())){
+                    shortcutMatchScore += partialTagMatchPoints
                 }
             }
         }
 
-        return null;
+        return shortcutMatchScore
     }
+    // static checkMatch(shortcut: KeyboardShortcut, searchTerm: string): Priority | null {
+
+    //     //check for perfect match to with function name
+    //     if (shortcut.text.toLocaleLowerCase() === searchTerm.toLocaleLowerCase()){
+    //         return Priority.PerfectNameMatch
+    //     }
+
+    //     //check if function name starts with the search term
+    //     if (shortcut.text.toLocaleLowerCase().startsWith(searchTerm.toLocaleLowerCase())){
+    //         return Priority.NameStartsWith
+    //     }
+
+    //     //check for simple tag matches
+    //     let perfectTagMatch = false
+    //     let TagStartsWith = false
+    //     for(const tag of shortcut.tags){
+    //         // check for perfect tag matchs
+    //         if (tag.toLocaleLowerCase() === searchTerm.toLocaleLowerCase()){
+    //             perfectTagMatch = true;
+    //             break
+    //         }
+
+    //         // check if the tag starts with the search term
+    //         if (tag.startsWith(searchTerm.toLocaleLowerCase())){
+    //             TagStartsWith = true;
+    //         }
+    //     }
+    //     if(perfectTagMatch){
+    //         return Priority.PerfectTagMatch
+    //     }else if(TagStartsWith){
+    //         return Priority.TagStartsWith
+    //     }
+
+
+    //     // the rest can stay
+
+    //     // split both the shortcut text and search term into individual words
+    //     const shortcutWords: string[] = shortcut.text.split(' ');
+    //     const searchWords: string[] = searchTerm.split(' ').filter(Boolean);
+
+    //     for (const searchWord of searchWords){
+            
+    //         // check for match against words in shortcut
+    //         for(const shortcutWord of shortcutWords){
+    //             if(shortcutWord.toLocaleLowerCase() === searchWord.toLocaleLowerCase()){
+    //                 return Priority.NameIncludes;
+    //             }else if(shortcutWord.toLocaleLowerCase().startsWith(searchWord.toLocaleLowerCase())){
+    //                 return Priority.NamePartial;
+    //             }
+    //         }
+
+    //         // check for match against shortcut tags
+    //         for(const tag of shortcut.tags){
+    //             if(searchTerm.toLocaleLowerCase() === tag.toLocaleLowerCase()){
+    //                 return Priority.TagIncludes;
+    //             }else if(tag.toLocaleLowerCase().startsWith(searchWord.toLocaleLowerCase())){
+    //                 return Priority.TagPartial;
+    //             }
+    //         }
+    //     }
+
+    //     return null;
+    // }
 
     // close QuickActions and run the shortcut
     static executeQuickAction(shortcut: KeyboardShortcut): void {
