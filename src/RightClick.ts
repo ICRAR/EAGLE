954e3f8213c2d4f446af3048b4052c3c484d039d
MWicenec <wicenecmw@gmail.com>
2025-07-07 10:57:04 +0800

    added right click options for comment and description

--- src/RightClick.ts
+++ src/RightClick.ts
@@ -620,7 +620,8 @@ export class RightClick {
                     $('#customContextMenu').append('<a onclick=RightClick.editNodeFuncCode()>Edit Function Code</a>')
                 }
                 $('#customContextMenu').append('<a onclick="ParameterTable.openTable(Eagle.BottomWindowMode.NodeParameterTable, ParameterTable.SelectType.RightClick)">Open Fields Table</a>')
-                $('#customContextMenu').append('<a onclick="ParameterTable.openTable(Eagle.BottomWindowMode.ConfigParameterTable, ParameterTable.SelectType.RightClick)">Graph Attributes</a>')
+                $('#customContextMenu').append('<a onclick="eagle.editNodeDescription()">Edit Description</a>')
+                $('#customContextMenu').append('<a onclick="eagle.editNodeComment()">Edit Comment</a>')
                 if (data.isConstruct()){
                     $('#customContextMenu').append('<a onclick=eagle.deleteSelection(true,false,true)>Delete with children</a>')
                     $('#customContextMenu').append('<a onclick=GraphRenderer.centerConstruct(eagle.selectedNode(),eagle.logicalGraph().getNodes())>Center Around Children</a>')
